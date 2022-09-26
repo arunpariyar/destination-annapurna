f06c5e04ccdf95872d6ca31aeef81913492a6260
Arun Pariyar <arun.pariyar@gmail.com>
2022-09-26 13:29:39 +0545

    :sparkles: add sass and first component
    sass extension is now applied, css reset and css architecture is also applied

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav">
+    <div class="nav_logo">
+      <img class="logo" src="images/logo.svg" alt="Mountains" />
+    </div>
+    <ul class="nav__links">
+      <li class="nav__item">
+        <a class="nav__link" href="#section--1">Adventures</a>
+      </li>
+      <li class="nav__item">
+        <a class="nav__link" href="#section--2">Impressions</a>
+      </li>
+      <li class="nav__item">
+        <a class="nav__link" href="#">Make it happen</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+<style></style>
